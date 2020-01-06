0deabe0fa964977be48e9e6871d454a9da9dae05
EzzEddin <ezz@localhost.localdomain>
2020-01-07 00:49:51 +0200

    initial commit

--- classicle_model.py
+++ classicle_model.py
@@ -8,7 +8,7 @@ def classicle_model(seed_text, model_name, max_length=120,padding_type='post'):
     with open(os.path.join(os.path.dirname(__file__), 'tokenizer.pickle'), 'rb') as handle:
         tokenizer = pickle.load(handle)
 
-    # loading the model: embedding, average pooling, and dense layers
+    # loading the model
     model = load_model(os.path.join(os.path.dirname(__file__), model_name + '.h5'))
     token_list = tokenizer.texts_to_sequences([seed_text])[0]
     token_list_padded = pad_sequences([token_list], maxlen=max_length, padding=padding_type)
@@ -18,6 +18,7 @@ def classicle_model(seed_text, model_name, max_length=120,padding_type='post'):
     with open(os.path.join(os.path.dirname(__file__), 'label_word_index.pickle'), 'rb') as handle:
         label_word_index = pickle.load(handle)
     
+    # getting the right category out of the five indexes
     for label in label_word_index.values():
       if predicted[0] == label:
         category = list(label_word_index.keys())[label-1]
